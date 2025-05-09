4383377ebe8f60778594464d3e71744815538045
David Getial <davidgetial2006@gmail.com>
2025-05-08 20:18:15 -0500

    arreglos

--- src/home.php
+++ src/home.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
+    <center>
+        <h1> WELCOME TO HOME</h1>
     <a href = "signin.html">Logout</a>
+    </center>
 </body>
 </html>
